1d68091363f3167fae5f887dae11fb5d09c877f3
Kraisorn.R <kraisorn204@gmail.com>
2025-09-22 10:03:10 +0700

    Update Program.cs

--- BaseClassRepository/BaseClassRepository/Program.cs
+++ BaseClassRepository/BaseClassRepository/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using BaseClassRepository.ApplicationCore.Abstractions.Repositories;
+using BaseClassRepository.ApplicationCore.Data;
+using BaseClassRepository.Infrastructure.Repositories;
+
+using var db = new BloggingContext();
+
+var postRepository = (IPostRepository)new PostRepository(db);
+
+var items = await postRepository.GetPostByMultipleBlogIdAsync([1, 2, 3]);
+
+foreach (var item in items)
+{
+    Console.WriteLine($"PostId: {item.PostId}, Title: {item.Title}, BlogId: {item.BlogId}");
+}
+
+var items2 = await postRepository.GetPostLookupAsync();
+
+foreach (var kvp in items2)
+{
+    Console.WriteLine($"PostId: {kvp.Id}, Title: {kvp.Value}");
+}
