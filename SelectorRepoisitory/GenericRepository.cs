cb73e873aa477f28e40d491f948ad9de32949fdf
Kraisorn.R <kraisorn204@gmail.com>
2025-10-13 12:56:22 +0700

    Add custom select of repository

---
+++ SelectorRepoisitory/GenericRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using System.Linq.Expressions;
+
+namespace SelectorRepoisitory;
+
+public class GenericRepository<TEntity> where TEntity : class
+{
+    private BloggingContext _context { get; init; }
+
+    public GenericRepository(BloggingContext context)
+    {
+        _context = context;
+    }
+
+    public IQueryable<TSelector> ListAsync<TSelector>(
+        Expression<Func<TEntity, TSelector>> select,
+        Expression<Func<TEntity, bool>>? filter = null,
+        Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
+        Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
+        bool asNoTracking = true,
+        CancellationToken ct = default)
+    {
+        var query = _context.Set<TEntity>().AsQueryable();
+
+        if (asNoTracking)
+        {
+            query = query.AsNoTracking();
+        }
+
+        if (include is not null)
+        {
+            query = include(query);
+        }
+
+        if (filter is not null)
+        {
+            query = query.Where(filter);
+        }
+
+        if (orderBy is not null)
+        {
+            query = orderBy(query);
+        }
+
+        return query.Select(select);
+    }
+}
---
+++ SelectorRepoisitory/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+using SelectorRepoisitory;
+
+Console.WriteLine("Hello, World!");
+
+var conn = new BloggingContext();
+
+var repo = new GenericRepository<Blog>(conn);
+
+// defualt select all
+var query = repo.ListAsync(
+    select: a => a,
+    orderBy: a => a.OrderBy(b => b.BlogId));
+
+Console.WriteLine(query.ToQueryString());
+
+var query2 = repo.ListAsync(
+    select: a => new BlogDto(a.BlogId, a.Url),
+    orderBy: a => a.OrderBy(b => b.BlogId));
+
+Console.WriteLine(query2.ToQueryString());
+
+public record BlogDto(int Id, string Name);
